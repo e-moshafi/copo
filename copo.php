293c2b0bfdb65c982b2bd4df9eb62d26efe853e1
erfanmoshafi <erfan.moshafi2018@gmail.com>
2021-10-29 19:49:38 +0330

    copo fixed

--- copo.php
+++ copo.php
@@ -1,15 +1,15 @@
 <?php
 /*
 Plugin Name: copo
-Plugin URI:   https://www.wpbeginner.com 
-Description:  A short little description of the plugin. It will be displayed on the Plugins page in WordPress admin area. 
+Plugin URI:   https://www.copo.erfanmoshafi.com 
+Description: copo for calendar program. 
 Version:      1.0
-Author:       WPBeginner 
-Author URI:   https://www.wpbeginner.com
-License:      GPL2
-License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  wpb-tutorial
-Domain Path:  /languages
+Author:       erfan moshafi 
+Author URI:   https://www.erfanmoshafi.com
+License:      GPL3
+License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:  
+Domain Path:  
 */
 require_once(ABSPATH.'wp-admin/includes/upgrade.php');
 include "function.php";
